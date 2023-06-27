e2dff7c0da54be8c908d88bc427cdae7b013ce76
Tsai-Ching <108188981+Tsai-Ching@users.noreply.github.com>
2023-06-27 13:16:00 +0800

    change database host name

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    host : dpg-cid3udtiuie2ea2d1gag-a,
     port : 5432,
     user : 'emmahsu',
     password : '8bBnoGpb4deK9kU4Dale55Gp59QcDwK1',
