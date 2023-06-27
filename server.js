83f54acbe02be083900f6c991bb45ed187b2266e
Tsai-Ching <108188981+Tsai-Ching@users.noreply.github.com>
2023-06-27 13:05:41 +0800

    connect to render database again

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env("DATABASE_URL"),
+    host : process.env.DATABASE_URL,
     port : 5432,
     user : 'emmahsu',
     password : '8bBnoGpb4deK9kU4Dale55Gp59QcDwK1',
