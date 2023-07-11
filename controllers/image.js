8ca2266dd26c475d494dd273597f141bbb2a612e
Tsai-Ching <108188981+Tsai-Ching@users.noreply.github.com>
2023-07-12 07:19:49 +0800

    set enviroment variable

--- controllers/image.js
+++ controllers/image.js
@@ -5,7 +5,7 @@ const {Configuration, OpenAIApi} = require("openai");
 const handleApiCall = (req, res) => {
 	const configuration = new Configuration({
 		organization: "org-JmXBPuadpIdZyXuR8FmOqYFf",
-		apiKey: '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',
+		apiKey: process.env.OPENAI_API_KEY,
 	});
 
 	const openai = new OpenAIApi(configuration);
